82a75301c0452201a83972c461b7dbee1892fbcb
victoralmeida92 <victor.almeida432@gmail.com>
2024-08-17 23:29:41 -0300

    changes on index.ts to call express correctly and remap on appRoutes to me sure register all routes properly

--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,31 @@
 import "reflect-metadata";
-import {createConnection} from "typeorm";
-import {Request, Response} from "express";
-import * as express from "express";
+import { createConnection, Connection } from "typeorm";
+import { Request, Response } from "express";
+import express, { Express } from "express";
 import * as bodyParser from "body-parser";
-import {AppRoutes} from "./routes";
+import { AppRoutes } from "./routes";
 
-// create connection with database
-// note that it's not active database connection
-// TypeORM creates connection pools and uses them for your requests
-createConnection().then(async connection => {
-
-    // create express app
-    const app = express();
+createConnection()
+  .then(async (connection: Connection) => {
+    const app: Express = express();
     app.use(bodyParser.json());
 
-    // register all application routes
-    AppRoutes.forEach(route => {
-        app[route.method](route.path, (request: Request, response: Response, next: Function) => {
-            route.action(request, response)
-                .then(() => next)
-                .catch(err => next(err));
-        });
+    AppRoutes.forEach((route) => {
+      const method = route.method as keyof Express;
+      (app[method] as Function)(
+        route.path,
+        (request: Request, response: Response, next: Function) => {
+          route
+            .action(request, response)
+            .then(() => next())
+            .catch((err) => next(err));
+        },
+      );
     });
 
     // run app
     app.listen(3000);
 
     console.log("Express application is up and running on port 3000");
-
-}).catch(error => console.log("TypeORM connection error: ", error));
+  })
+  .catch((error) => console.log("TypeORM connection error: ", error));
